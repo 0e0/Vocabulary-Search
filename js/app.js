1c9cc9d2c3c9921a5acb8bab0a2e8aaad195bcd2
0e0 <ejersonb@gmail.com>
2016-07-28 14:16:15 -0500

    Work on API for flickr

--- js/app.js
+++ js/app.js
@@ -1,48 +1,71 @@
-//PLAN of steps to do 
+//PLAN of steps to do secretkey: 54f516ad437cbc7a
 $(function() {
 
-    //STEP 1 : get the data from API youtube 
-    function getImage(searchTerm) {
-        var url = 'https://ajax.googleapis.com/ajax/services/search/images';
+    //STEP 1 : get the data from API youtube (Remember to place the searchTerm back as a parameter)
+    function getImage() {
+        var url = 'https://api.flickr.com/services';
         var params = {
-            part: 'snippet',
-            key: 'AIzaSyCQQNC20CuEMQVuJSe-NVyZ9NXZHUbWlgc',
-            q: searchTerm,
-            v: 1.0,
-            maxResults: 10
+            // format: json,
+            api_key: 'c462c4f54e7fd6cafdc94532bbdb69d5',
+            q: 'monkey',
+            // searchType: 'image',
+            maxResults: 10,
+            // method: 'search'
         };
 
-        $.getJSON(url, params, function(data) {
+        $.getJSON(url + '&callback=?', params, function(data) {
             //STEP 2 : Analize the API Data  
-            console.log(data.items);
-            showData(data.items);
+            console.log(data)
+            // console.log(data.items);
+            // showData(data.items);
         });
     }
+getImage();
+    
+// <script>
+//   (function() {
+//     var cx = '008577567035179202735:rfbaqfoibuw';
+//     var gcse = document.createElement('script');
+//     gcse.type = 'text/javascript';
+//     gcse.async = true;
+//     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(gcse, s);
+//   })();
+// </script>
+// <gcse:search></gcse:search>
+
+
+
+
 
     // STEP 3 : Show/render it to HTML 
-    function showData(result) {
-        var html = '';
+//     function showData(result) {
+//         var html = '';
 
-        $.each(result, function(index, value) {
-            html += '<a href="https://www.google.com/imghp?hl=en&tab=wi&ei=OzaaV6PZL8ex-QGL-KGICg&ved=0EKouCBYoAQ='+ value.id.videoId + ' "> <img src="' + 
-            value.snippet.thumbnails.high.url + '" class="col-xs-12 col-md-4 img-responsive"></a> <h5>'+ 
-            value.snippet.title +'</h5>';
-        });
-        $("#resultsContainer").append(html);
-    }
+//         $.each(result, function(index, value) {
+//             html += '<a href="https://www.google.com/imghp?hl=en&tab=wi&ei=OzaaV6PZL8ex-QGL-KGICg&ved=0EKouCBYoAQ='+ value.id.videoId + ' "> <img src="' + 
+//             value.snippet.thumbnails.high.url + '" class="col-xs-12 col-md-4 img-responsive"></a> <h5>'+ 
+//             value.snippet.title +'</h5>';
+//         });
+//         $("#resultsContainer").append(html);
+//     }
 
-    //STEP 4 : Create an Event handler 
-    //that on click calls our data and Show Data functions
+ //STEP 4 : Create an Event handler 
+//that on click calls our data and Show Data functions
 
-    $('form').submit(function(e) {
-        e.preventDefault();
-        var searchTerm = $('#searchTerm').val();
-        console.log(searchTerm);
-        getData(searchTerm);
-        $('#searchTerm').val('');
+//     $('form').submit(function(e) {
+//         e.preventDefault();
+//         var searchTerm = $('#searchTerm').val();
+//         console.log(searchTerm);
+//         getData(searchTerm);
+//         $('#searchTerm').val('');
 
         //alert(searchTerm);
 
-    });
+//     });
+
+ // part: 'snippet',
+            // cx: '008577567035179202735:rfbaqfoibuw'
 
 });
