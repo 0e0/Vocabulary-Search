849f7a8d933071cc1a8ce867e217aa14bfab8f9d
0e0 <ejersonb@gmail.com>
2016-07-28 13:15:03 -0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+//PLAN of steps to do 
+$(function() {
+
+    //STEP 1 : get the data from API youtube 
+    function getImage(searchTerm) {
+        var url = 'https://ajax.googleapis.com/ajax/services/search/images';
+        var params = {
+            part: 'snippet',
+            key: 'AIzaSyCQQNC20CuEMQVuJSe-NVyZ9NXZHUbWlgc',
+            q: searchTerm,
+            v: 1.0,
+            maxResults: 10
+        };
+
+        $.getJSON(url, params, function(data) {
+            //STEP 2 : Analize the API Data  
+            console.log(data.items);
+            showData(data.items);
+        });
+    }
+
+    // STEP 3 : Show/render it to HTML 
+    function showData(result) {
+        var html = '';
+
+        $.each(result, function(index, value) {
+            html += '<a href="https://www.google.com/imghp?hl=en&tab=wi&ei=OzaaV6PZL8ex-QGL-KGICg&ved=0EKouCBYoAQ='+ value.id.videoId + ' "> <img src="' + 
+            value.snippet.thumbnails.high.url + '" class="col-xs-12 col-md-4 img-responsive"></a> <h5>'+ 
+            value.snippet.title +'</h5>';
+        });
+        $("#resultsContainer").append(html);
+    }
+
+    //STEP 4 : Create an Event handler 
+    //that on click calls our data and Show Data functions
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        var searchTerm = $('#searchTerm').val();
+        console.log(searchTerm);
+        getData(searchTerm);
+        $('#searchTerm').val('');
+
+        //alert(searchTerm);
+
+    });
+
+});
