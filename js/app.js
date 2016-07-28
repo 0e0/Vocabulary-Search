d3074bb9d1b38f848cd17014f69f841cf8832333
0e0 <ejersonb@gmail.com>
2016-07-28 16:30:54 -0500

    Need to iterate through the array

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
 function jsonFlickrFeed(json) {
     console.log(json);
-  };
+     var myData = json.items[0].media.m;
+        console.log(myData);
+};
 
-  $(document).ready(function() {
+// function showData(json) {
+//         var html = '';
+       
 
-    $.ajax({
-      url: 'https://api.flickr.com/services/feeds/photos_public.gne',
-      dataType: 'jsonp',
-      data: {
-        "tags": "kitten",
-        "format": "json"
-      }
+//       console.log(value.media);
+//         $.each(json, function(index, value) {
+//         html += '<p>'+ value.media.m + '</p>' ;
+//         });
+//         $("#resultsContainer").append(html);
+//     };
+// // showData();
+
+
+    $(document).ready(function() {
+
+        $.ajax({
+            url: 'https://api.flickr.com/services/feeds/photos_public.gne',
+            dataType: 'jsonp',
+            data: {
+                "tags": "football",
+                "format": "json"
+            }
+        });
+
+
     });
 
-  });
+    
