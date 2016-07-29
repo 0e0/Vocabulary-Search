dd32a7ab7c5807b771c9b337a77d8d7f24c6584d
bjf2015 <bfranzoi@icloud.com>
2016-07-29 12:05:12 -0700

    yikes

--- js/app.js
+++ js/app.js
@@ -19,6 +19,9 @@ function getData(searchTerm) {
             data: {
                 tags: searchTerm,
                 format: 'json'
+
+
+
             }
         });
 
--- js/forvo.js
+++ js/forvo.js
@@ -1,28 +1,44 @@
-function forvoData(json){
-	console.log(json)
-  var myData = json.items[0];
-  console.log(myData);
+function forvoData(json) {
+    console.log(json)
+    var myData = json.items[0];
+    console.log(myData);
+    var html = '';
+    $("#forvo-results-container").empty();
+    $.each(json.items, function(index, value) {
+        html += '<img src="' + value.media.m 
+        + '"class="col-xs-12 col-md-4 img-responsive">';
+    });
+    $("#forvo-results-container").append(html);
 };
 
-function getData() {
+function getData(searchTerm) {
 
-        $.ajax({
-            url: 'http://apifree.forvo.com/',
-            dataType: 'jsonp',
-            data: {
-            	action: 'word-pronunciations',
-            	format: 'json',
-            	word: 'hund',
-                language: 'de',
-                key: '0f50e41f73f4e8ef682496c136e02994'
-            }	
+    $.ajax({
+        url: 'http://apifree.forvo.com/',
+        dataType: 'jsonp',
+        action: 'word-pronunciations',
+        format: 'json',
+        word: searchTerm,
+        language: 'de',
+        key: '0f50e41f73f4e8ef682496c136e02994'
         });
-}
+    
+    };
 
 
+//}
 
+
+$('form').submit(function(e){
+    e.preventDefault();
+    var searchTerm = $('#search-term').val();
+    console.log(searchTerm);
+    getData(searchTerm);
+    $('#search-term').val('');
+
+});
 // $.getJSON('http://apifree.forvo.com/action/word-pronunciations/format/json/word/hund/language/de/key/0f50e41f73f4e8ef682496c136e02994/&callback=?', function(data){
-  
+
 //   var myData = data.items;
 //   console.log(myData);
 // });
