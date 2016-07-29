2d71797ecce25d292da8dc1304b8df9b83625bba
0e0 <ejersonb@gmail.com>
2016-07-29 10:08:40 -0500

    Flickr is Working, Yey

--- js/app.js
+++ js/app.js
@@ -1,34 +1,41 @@
 function jsonFlickrFeed(json) {
     console.log(json);
-     var myData = json.items[0].media.m;
-        console.log(myData);
+    var myData = json.items[0].media.m;
+    console.log(myData);
+    var html = '';
+    $("#results-container").empty();
+    $.each(json.items, function(index, value) {
+    html += '<img src="'+ value.media.m
+        +'"class="col-xs-12 col-md-4 img-responsive">';
+        });
+        $("#results-container").append(html);
 };
 
-// function showData(json) {
-//         var html = '';
-       
-
-//       console.log(value.media);
-//         $.each(json, function(index, value) {
-//         html += '<p>'+ value.media.m + '</p>' ;
-//         });
-//         $("#resultsContainer").append(html);
-//     };
-// // showData();
-
-
-    $(document).ready(function() {
+function getData(searchTerm) {
 
         $.ajax({
             url: 'https://api.flickr.com/services/feeds/photos_public.gne',
             dataType: 'jsonp',
             data: {
-                "tags": "football",
-                "format": "json"
+                tags: searchTerm,
+                format: 'json'
             }
         });
 
 
-    });
+}
+
+
+$('form').submit(function(e){
+    e.preventDefault();
+    var searchTerm = $('#search-term').val();
+    console.log(searchTerm);
+    getData(searchTerm);
+    $('#search-term').val('');
+
+});
+
+
+   
 
     
